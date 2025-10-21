6a2c7400a586d6d1cf0032961acde2f2e5500ddf
LeeKM321 <stephen4951@gmail.com>
2025-10-21 10:51:50 +0900

    Book, Member, Rental 엔터티 클래스 작성

---
+++ src/main/java/com/codeit/springwebbasic/member/entity/MemberGrade.java
@@ -0,0 +1,25 @@
+package com.codeit.springwebbasic.member.entity;
+
+public enum MemberGrade {
+    BRONZE(0),
+    SILVER(5),
+    GOLD(15),
+    PLATINUM(30);
+
+    private final int requiredRentals;
+
+    MemberGrade(int requiredRentals) {
+        this.requiredRentals = requiredRentals;
+    }
+
+    public MemberGrade upgrade() {
+        // 새로운 switch 표현식
+        return switch (this) {
+            case BRONZE -> SILVER;
+            case SILVER -> GOLD;
+            case GOLD -> PLATINUM;
+            case PLATINUM -> PLATINUM;
+        };
+    }
+
+}
