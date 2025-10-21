1abc99d70be54590f7933dcb3a423a4cfb789ad7
LeeKM321 <stephen4951@gmail.com>
2025-10-21 15:51:47 +0900

    Book Controller, Service 생성 및 의존성 주입, Builder 패턴 적용해 보기

---
+++ src/main/java/com/codeit/springwebbasic/book/service/BookService.java
@@ -0,0 +1,43 @@
+package com.codeit.springwebbasic.book.service;
+
+import com.codeit.springwebbasic.book.dto.request.BookCreateRequestDto;
+import com.codeit.springwebbasic.book.entity.Book;
+import com.codeit.springwebbasic.book.entity.BookStatus;
+import com.codeit.springwebbasic.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public void createBook(BookCreateRequestDto requestDto) {
+        // ISBN 중복 체크
+        Optional<Book> byIsbn = bookRepository.findByIsbn(requestDto.getIsbn());
+        if (byIsbn.isPresent()) {
+            throw new IllegalArgumentException("이미 등록된 ISBN입니다: " + requestDto.getIsbn());
+        }
+
+        // Builder 패턴을 활용하면 초기화 순서를 내 맘대로 지정해도 상관 없고,
+        // 원하는 필드만 골라서 초기화 하는것이 가능.
+        Book book = Book.builder()
+                .status(BookStatus.AVAILABLE)
+                .author(requestDto.getAuthor())
+                .isbn(requestDto.getIsbn())
+                .title(requestDto.getTitle())
+                .publisher(requestDto.getPublisher())
+                .publishedDate(requestDto.getPublishedDate())
+                .build();
+
+
+        bookRepository.save(book);
+
+
+    }
+
+
+}
